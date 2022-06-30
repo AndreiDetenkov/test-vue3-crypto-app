a2d3be74a3170d30dd7653244600cce46edd3564
AndreiDetenkov <andrei.detenkov@somo.kg>
2022-07-01 00:40:00 +0600

    feat: add withdrawal form

--- src/views/withdraw/WithdrawalView.vue
+++ src/views/withdraw/WithdrawalView.vue
@@ -4,9 +4,14 @@ import { useWalletStore } from '@/stores/wallet'
 import { storeToRefs } from 'pinia'
 import { COIN_ICONS_PATH } from '@/utils/consts'
 import useCoin from '@/composables/useCoin'
+import { onMounted } from 'vue'
 
 const walletStore = useWalletStore()
 const { walletCoin, walletData } = storeToRefs(walletStore)
+
+onMounted((): void => {
+  walletStore.fetchWalletData()
+})
 
 const { setCoin } = useCoin()
 </script>
@@ -40,60 +45,87 @@ const { setCoin } = useCoin()
 
       <div class="flex-1 border p-6">
         <div
-          class="flex items-center justify-between bg-gray-100 px-8 py-4 mb-6"
+          class="flex items-center justify-between bg-gray-100 px-8 py-4 mb-12"
         >
-          <div class="flex items-center">
-            <img
-              :src="`${COIN_ICONS_PATH}/${walletCoin.icon}`"
-              alt="coin icon"
-              class="w-8 h-8"
-            />
-            <span class="pl-4 text-2xl font-bold">{{ walletCoin.title }}</span>
-          </div>
-          <div class="text-gray-500">
-            Available balance: {{ walletCoin.available }}
-            <span class="uppercase">{{ walletCoin.short }}</span>
-          </div>
+          <template v-if="Object.keys(walletCoin).length">
+            <div class="flex items-center">
+              <img
+                :src="`${COIN_ICONS_PATH}/${walletCoin.icon}`"
+                alt="coin icon"
+                class="w-8 h-8"
+              />
+              <span class="pl-4 text-2xl font-bold">
+                {{ walletCoin.title }}
+              </span>
+            </div>
+            <div class="text-gray-500">
+              Available balance: {{ walletCoin.available }}
+              <span class="uppercase">{{ walletCoin.short }}</span>
+            </div>
+          </template>
+          <template v-else>
+            <span class="text-2xl">Please select coin</span>
+          </template>
         </div>
 
-        <div>
-          <div class="flex mb-6">
-            <div class="w-1/4">
-              <label for="inline-full-name"> Address </label>
+        <div class="flex justify-center">
+          <form @submit.prevent class="w-3/4">
+            <div class="flex mb-6">
+              <div class="w-1/4">
+                <label for="address"> Address </label>
+              </div>
+              <div class="w-3/4">
+                <input
+                  class="form-input border border-gray-200 rounded w-full py-2 px-4"
+                  id="address"
+                  type="text"
+                  placeholder="Enter address here"
+                />
+              </div>
             </div>
-            <div class="w-3/4">
-              <input
-                class="form-input border border-gray-200 rounded w-full py-2 px-4"
-                id="inline-full-name"
-                type="text"
-              />
-            </div>
-          </div>
-          <div class="flex mb-6">
-            <div class="w-1/4">
-              <label for="inline-full-name"> Amount </label>
-            </div>
-            <div class="w-3/4">
-              <input
-                class="form-input border border-gray-200 rounded w-full py-2 px-4"
-                id="inline-full-name"
-                type="text"
-              />
-            </div>
-          </div>
 
-          <div class="flex mb-6">
-            <div class="w-1/4">
-              <label for="inline-full-name"> Comment </label>
+            <div class="flex mb-6">
+              <div class="w-1/4">
+                <label for="network"> Network </label>
+              </div>
+              <div class="w-3/4">
+                <select
+                  class="form-select border border-gray-200 rounded w-full py-2 px-4"
+                  id="network"
+                >
+                  <option selected>Select withdrawal network</option>
+                  <option>Option 1</option>
+                  <option>Option 2</option>
+                </select>
+              </div>
             </div>
-            <div class="w-3/4">
-              <textarea
-                rows="4"
-                class="form-textarea border border-gray-200 rounded w-full py-2 px-4"
-                id="inline-full-name"
-              />
+
+            <div class="flex mb-6">
+              <div class="w-1/4">
+                <label for="amount"> Amount </label>
+              </div>
+              <div class="w-3/4">
+                <input
+                  class="form-input border border-gray-200 rounded w-full py-2 px-4"
+                  id="amount"
+                  type="text"
+                />
+              </div>
             </div>
-          </div>
+
+            <div class="flex mb-6">
+              <div class="w-1/4">
+                <label for="comment"> Comment </label>
+              </div>
+              <div class="w-3/4">
+                <textarea
+                  rows="4"
+                  class="form-textarea border border-gray-200 rounded w-full py-2 px-4"
+                  id="comment"
+                />
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>

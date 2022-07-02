637426ada1e58c1cfd1b5c49b1e1d8b23d507776
AndreiDetenkov <andrei.detenkov@somo.kg>
2022-07-02 22:52:25 +0600

    feat: add confirmation form and info block

--- src/views/withdraw/steps/WithdrawalConfirmation.vue
+++ src/views/withdraw/steps/WithdrawalConfirmation.vue
@@ -1,5 +1,49 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useWithdrawStore } from '@/stores/withdraw'
+import { storeToRefs } from 'pinia'
+import AppInput from '@/components/app/AppInput.vue'
 
-<template>confirm</template>
+const withdrawStore = useWithdrawStore()
+const { withdrawDetails } = storeToRefs(withdrawStore)
+</script>
 
-<style scoped lang="scss"></style>
+<template>
+  <div class="sm:w-full lg:w-3/4 xl:w-2/4 mx-auto bg-gray-100 p-4 mb-10">
+    <div>
+      <span class="inline-block w-1/3">Amount</span>
+      <span class="">Total 0.30401394 BNB (Network fee 0.0005 BNB)</span>
+    </div>
+    <div>
+      <span>Address</span>
+      <span>bc1qxy2kgdygjrsqtzq2n0yrf2493p83kkfjhx0wl</span>
+    </div>
+    <div>
+      <span>Network</span>
+      <span>BNB Smart Chain (BEP20)</span>
+    </div>
+  </div>
+
+  <form class="sm:w-full lg:w-3/4 xl:w-2/4 mx-auto flex flex-col gap-6">
+    <fieldset>
+      <app-input type="password" label="Password" />
+    </fieldset>
+
+    <fieldset>
+      <app-input label="Phone verification code" class="relative" />
+    </fieldset>
+
+    <fieldset class="flex mt-4">
+      <button
+        class="border border-primary p-3 mr-4 rounded font-medium hover:bg-gray-100 w-1/2"
+      >
+        Cancel
+      </button>
+      <button
+        type="submit"
+        class="bg-primary text-gray-300 p-3 ml-4 rounded font-medium hover:bg-primary/90 w-1/2"
+      >
+        Submit
+      </button>
+    </fieldset>
+  </form>
+</template>

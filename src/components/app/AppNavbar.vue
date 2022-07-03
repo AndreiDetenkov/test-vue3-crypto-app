099dcba03b76c2068fd0a5e522c80e1498c5fb8e
AndreiDetenkov <andrei.detenkov@somo.kg>
2022-07-03 14:10:30 +0600

    feat: update nav

--- src/components/app/AppNavbar.vue
+++ src/components/app/AppNavbar.vue
@@ -1,62 +1,29 @@
 <script setup lang="ts">
 import type { MenuInterface } from '@/components/app/types'
 import { web_route } from '@/utils/webConfig'
-import WalletIcon from '@/components/wallet/WalletIcon.vue'
 
 const menu: MenuInterface[] = [
-  { title: 'Wallet', url: web_route.wallet, icon: 'wallet.svg' },
+  { title: 'Wallet', url: web_route.wallet },
+  { title: 'History', url: web_route.history },
 ]
 </script>
 
 <template>
-  <nav class="nav">
-    <div class="nav__container">
-      <router-link to="/" class="logo"> Logo </router-link>
+  <nav class="h-16 bg-primary flex items-center">
+    <div class="container mx-auto flex items-center justify-between px-4">
+      <router-link to="/" class="text-2xl text-white"> Logo </router-link>
 
-      <div class="menu">
+      <div class="flex justify-center">
         <router-link
           v-for="item in menu"
           :key="item.title"
           :to="item.url"
-          class="menu__link"
-          active-class="active"
+          class="h-16 px-4 mx-2 flex items-center justify-center text-gray-300 text-center uppercase text-sm font-medium transition duration-300 ease-in-out;"
+          active-class="text-accent font-bold border-b-accent bg-amber-400/10"
         >
-          <wallet-icon class="menu__icon" />
           {{ item.title }}
         </router-link>
       </div>
     </div>
   </nav>
 </template>
-
-<style scoped lang="scss">
-.nav {
-  @apply h-16 bg-primary flex items-center;
-
-  &__container {
-    @apply container mx-auto flex items-center justify-between px-4;
-  }
-}
-
-.logo {
-  @apply text-2xl text-white;
-}
-
-.menu {
-  @apply flex justify-center;
-
-  &__link {
-    @apply h-16 px-4 mx-2 flex items-center justify-center
-    text-gray-300 text-center uppercase text-sm font-medium
-    transition duration-300 ease-in-out;
-  }
-
-  &__icon {
-    @apply fill-default/[.33] mr-2 w-6 h-6;
-  }
-}
-
-.active {
-  @apply text-accent font-bold border-b-accent bg-amber-400/10;
-}
-</style>
--- src/components/app/types.ts
+++ src/components/app/types.ts
@@ -1,7 +1,6 @@
 export interface MenuInterface {
   title: string
   url: string
-  icon: string
 }
 
 export interface SelectOption {

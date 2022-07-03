49069fc2abfa9403e65b755b3181980a672be108
AndreiDetenkov <andrei.detenkov@somo.kg>
2022-07-03 17:29:57 +0600

    feat: add loader on the main page

--- src/views/main/MainView.vue
+++ src/views/main/MainView.vue
@@ -1,20 +1,22 @@
 <script setup lang="ts">
 import { onBeforeMount } from 'vue'
+import { storeToRefs } from 'pinia'
 import { useMarketStore } from '@/stores/markets'
 import { TABLE_HEADERS } from '@/components/main/utils'
 import AppPageTitle from '@/components/app/AppPageTitle.vue'
 import AppTable from '@/components/app/AppTable.vue'
-import { storeToRefs } from 'pinia'
+import AppLoader from '@/components/app/AppLoader.vue'
 
 const marketStore = useMarketStore()
-const { markets } = storeToRefs(marketStore)
+const { markets, isLoading } = storeToRefs(marketStore)
 onBeforeMount(() => {
   marketStore.fetchMarkets()
 })
 </script>
 
 <template>
-  <section class="main-container">
+  <app-loader v-if="isLoading" />
+  <section v-else class="main-container">
     <app-page-title>Markets</app-page-title>
 
     <app-table>
